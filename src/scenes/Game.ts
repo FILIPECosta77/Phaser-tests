204908832c0be2a37b903ddde01d0ef1790ad6fe
Filipe Costa <84087378+FILIPECosta77@users.noreply.github.com>
2024-04-23 13:19:24 -0300

    add: player assets, map assets and render map

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -11,28 +11,16 @@ export class Game extends Scene {
 
   create() {
     this.camera = this.cameras.main;
-    this.camera.setBackgroundColor(0x00ff00);
+    this.background = this.add.image(450, 256, "background");
 
-    this.background = this.add.image(512, 384, "background");
-    this.background.setAlpha(0.5);
-
-    this.msg_text = this.add.text(
-      512,
-      384,
-      "Make something fun!\nand share it with us:\nsupport@phaser.io",
-      {
-        fontFamily: "Arial Black",
-        fontSize: 38,
-        color: "#ffffff",
-        stroke: "#000000",
-        strokeThickness: 8,
-        align: "center",
-      }
+    const map = this.make.tilemap({ key: "map" });
+    // @ts-ignore
+    const renderTiles: Phaser.Tilemaps.Tileset = map!.addTilesetImage(
+      "Tiled",
+      "tiles"
     );
-    this.msg_text.setOrigin(0.5);
 
-    this.input.once("pointerdown", () => {
-      this.scene.start("GameOver");
-    });
+    const showMapBorder = map.createLayer("Border", renderTiles, 0, 0);
+    const showMapMain = map.createLayer("main", renderTiles, 0, 0);
   }
 }
